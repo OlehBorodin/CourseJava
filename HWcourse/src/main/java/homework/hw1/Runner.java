10bb98538ba4bbba3f44eb8b1fa5ba57a8f57878
Oleh Borodin <94630637+OlehBorodin@users.noreply.github.com>
2022-06-04 17:24:10 +0200

    Hw1

---
+++ HWcourse/src/main/java/homework/hw1/Runner.java
@@ -0,0 +1,15 @@
+package homework.hw1;
+
+public class Runner {
+    public static void main(String[] args) {
+      Easy.printArray();
+        System.out.println("___________________");
+        Easy.reversArray();
+
+        Easy.getArgument();
+        System.out.println("___________________");
+        Easy.getReversArgument();
+        High.palindrome();
+    }
+
+}
